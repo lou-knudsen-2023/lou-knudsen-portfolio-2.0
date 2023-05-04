a04a9e6bafec6a06a8fe43d47b63de24dfbe1149
Lou Knudsen <lou.knudsen.nz@gmail.com>
2023-05-05 10:06:34 +1200

    home and protip component set up

---
+++ client/components/Home.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react'
+import Container from '@mui/material/Container'
+import Typography from '@mui/material/Typography'
+import Box from '@mui/material/Box'
+// import Link from '@mui/material/Link'
+// import ProTip from './ProTip'
+
+export default function Home() {
+  return (
+    <Container maxWidth="sm">
+      <Box sx={{ my: 4 }}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Lou Knudsen Portfolio
+        </Typography>
+        {/* <ProTip /> */}
+      </Box>
+    </Container>
+  )
+}
