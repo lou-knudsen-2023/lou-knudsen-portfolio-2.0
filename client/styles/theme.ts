488dcd3e44c0b55006b1da286fbaed76479ec665
Lou Knudsen <lou.knudsen.nz@gmail.com>
2023-05-05 10:49:29 +1200

    some color palletes set up

--- client/styles/theme.ts
+++ client/styles/theme.ts
@@ -1,18 +1,49 @@
 import { createTheme } from '@mui/material/styles'
 import { red } from '@mui/material/colors'
 
+const newFont = "'Rajdhani', sans-serif;"
+
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#5e193d',
+      main: '#306359',
     },
     secondary: {
-      main: '#19857b',
+      main: '#7ABD9B',
     },
     error: {
       main: red.A400,
     },
   },
+  typography: {
+    fontFamily: newFont,
+    button: {
+      fontSize: '0.8rem',
+    },
+    subtitle2: {
+      color: '#D1EAC1',
+      fontSize: 15,
+      fontWeight: 'bold',
+    },
+    subtitle1: {
+      fontSize: 14,
+    },
+    body1: {
+      fontSize: 15,
+      fontWeight: 300,
+    },
+  },
 })
 
 export default theme
+
+// theme = responsiveFontSizes(theme)
+
+// THEME VARIABLES
+// .palette
+// .typography
+// .spacing
+// .breakpoints
+// .zIndex
+// .transitions
+// .components
