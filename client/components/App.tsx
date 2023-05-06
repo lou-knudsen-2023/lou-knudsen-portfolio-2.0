4c9d608baaea9173e18cb8a075600909930f1236
Lou Knudsen <lou.knudsen.nz@gmail.com>
2023-05-06 15:05:49 +1200

    hardcoding components to convert to json file soon

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,16 +1,30 @@
 import * as React from 'react'
+import { Route, Routes } from 'react-router-dom'
 import Nav from './Nav'
 import Home from './Home'
 import Footer from './Footer'
 import ProjectCard from './ProjectCard'
+import ProjectBuddy from './ProjectBuddy'
+import ProjectDevDeals from './ProjectDevDeals'
+import ProjectWeeMan from './ProjectWeeMan'
+import ProjectLostandFound from './ProjectLostandFound'
 
 export default function App() {
   return (
     <>
       <Nav />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/project-buddy" element={<ProjectBuddy />} />
+        <Route path="/project-devdeals" element={<ProjectDevDeals />} />
+        <Route path="/project-lostandfound" element={<ProjectLostandFound />} />
+        <Route path="/project-weeman" element={<ProjectWeeMan />} />
+      </Routes>
       <ProjectCard />
       <Footer />
     </>
   )
+}
+
+{
 }
