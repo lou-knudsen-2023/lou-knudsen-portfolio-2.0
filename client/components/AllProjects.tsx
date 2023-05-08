ef36a0235f9b7f30a78cd72f5ff0a4f3d73447a3
Lou Knudsen <lou.knudsen.nz@gmail.com>
2023-05-08 22:29:59 +1200

    got the images and the data presenting correctly on the front end - phew

--- client/components/AllProjects.tsx
+++ client/components/AllProjects.tsx
@@ -42,43 +42,78 @@ export default function AllProjects(props: ProjectsPostProps) {
       });
   }, []);
 
-
   return (
     <Container maxWidth='lg'>
-      <Grid container spacing={4}>
- 
-      {post.map((post) => (
-        <Grid key={post.id}>
-          <CardActionArea component="a" href="#" sx={{maxWidth: "350px"}}>
-            <Card sx={{ display: 'flex' }}>
-              <CardContent sx={{ flex: 1 }}>
-                <Typography component="h2" variant="h5">
-                  {post.name}
-                </Typography>
-                <Typography variant="subtitle1" color="text.secondary">
-                  {post.date}
-                </Typography>
-                <Typography variant="subtitle1" paragraph>
-                  {post.blurb}
-                </Typography>
-                <Typography variant="subtitle1" color="primary">
-                  Continue reading...
-                </Typography>
-              </CardContent>
-              <CardMedia
-                component="img"
-                sx={{ width: 160, display: { xs: 'none', sm: 'block' } }}
-                src={`data:image/png;base64, ${post.image}`}
-                alt={`a screenshot of the webpage for project ${post.name}`}
+      <Grid container spacing={6} justifyContent="center" >
 
-              />
-            </Card>
-          </CardActionArea>
-        </Grid>
-      ))}
+        {post.map((post) => (
+          <Grid key={post.id} item xs={12} sm={6} md={6} style={{width: '100%'}}>
+            <CardActionArea component="a" href="#">
+              <Card sx={{ display: 'flex', flexDirection: 'row', minHeight: '200px', minWidth: '350px' }}>
+                <CardContent sx={{ flex: 1 }}>
+                  <Typography component="h2" variant="h5">
+                    {post.name}
+                  </Typography>
+                  <Typography variant="subtitle1" color="text.secondary">
+                    {post.date}
+                  </Typography>
+                  <Typography variant="subtitle1" paragraph>
+                    {post.blurb}
+                  </Typography>
+                  <Typography variant="subtitle1" color="primary">
+                    Continue reading...
+                  </Typography>
+                </CardContent>
+                <CardMedia
+                  component="img"
+                  sx={{ width: 200, display: { xs: 'none', sm: 'block' } }}
+                  src={`data:image/png;base64, ${post.image}`}
+                  alt={`a screenshot of the webpage for project ${post.name}`}
+                />
+              </Card>
+            </CardActionArea>
+          </Grid>
+        ))}
       </Grid>
     </Container>
   )
-}
+        }
+  
+//   return (
+//     <Container maxWidth='lg'>
+//       <Grid container spacing={4}>
+ 
+//       {post.map((post) => (
+//         <Grid key={post.id}>
+//           <CardActionArea component="a" href="#" sx={{maxWidth: "350px"}}>
+//             <Card sx={{ display: 'flex' }}>
+//               <CardContent sx={{ flex: 1 }}>
+//                 <Typography component="h2" variant="h5">
+//                   {post.name}
+//                 </Typography>
+//                 <Typography variant="subtitle1" color="text.secondary">
+//                   {post.date}
+//                 </Typography>
+//                 <Typography variant="subtitle1" paragraph>
+//                   {post.blurb}
+//                 </Typography>
+//                 <Typography variant="subtitle1" color="primary">
+//                   Continue reading...
+//                 </Typography>
+//               </CardContent>
+//               <CardMedia
+//                 component="img"
+//                 sx={{ width: 160, display: { xs: 'none', sm: 'block' } }}
+//                 src={`data:image/png;base64, ${post.image}`}
+//                 alt={`a screenshot of the webpage for project ${post.name}`}
+//               />
+//             </Card>
+//           </CardActionArea>
+//         </Grid>
+//       ))}
+//       </Grid>
+//     </Container>
+//   )
+// }
 
 
