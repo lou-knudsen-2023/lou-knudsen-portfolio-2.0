360aa57cb6bfd8ddca03a775925692e114a2f0d2
Lou Knudsen <lou.knudsen.nz@gmail.com>
2023-05-09 09:58:06 +1200

    files working

--- client/components/AllProjects.tsx
+++ client/components/AllProjects.tsx
@@ -12,9 +12,12 @@ import {
 import { useEffect, useState } from 'react'
 import { getAllProjectsCS } from '../apis/apiClientExample'
 import { Projects } from '../../commons/model'
+import { useNavigate } from 'react-router-dom';
+
 
 interface ProjectsPostProps {
   post: {
+    id: Number
     name: string
     date: string
     blurb: string
@@ -32,6 +35,8 @@ interface ProjectsPostProps {
 export default function AllProjects(props: ProjectsPostProps) {
   const [post, setProjects] = useState<Projects[]>([])
 
+  const navigate = useNavigate();
+
   useEffect(() => {
     getAllProjectsCS()
       .then((data) => {
@@ -42,13 +47,18 @@ export default function AllProjects(props: ProjectsPostProps) {
       });
   }, []);
 
+  const handleNav = (id: Number) => {
+    navigate(`/api/v1/projects/${post.id}`)
+  }
+
   return (
     <Container maxWidth='lg'>
       <Grid container spacing={6} justifyContent="center" >
 
         {post.map((post) => (
           <Grid key={post.id} item xs={12} sm={6} md={6} style={{width: '100%'}}>
-            <CardActionArea component="a" href="#">
+            <CardActionArea component="a" >
+            <Link key={post.id} to={`/api/v1/projects/${post.id}`} style={{ textDecoration: 'none' }}>
               <Card sx={{ display: 'flex', flexDirection: 'row', minHeight: '200px', minWidth: '350px' }}>
                 <CardContent sx={{ flex: 1 }}>
                   <Typography component="h2" variant="h5">
@@ -71,6 +81,7 @@ export default function AllProjects(props: ProjectsPostProps) {
                   alt={`a screenshot of the webpage for project ${post.name}`}
                 />
               </Card>
+          </Link>
             </CardActionArea>
           </Grid>
         ))}
@@ -79,41 +90,3 @@ export default function AllProjects(props: ProjectsPostProps) {
   )
         }
   
-//   return (
-//     <Container maxWidth='lg'>
-//       <Grid container spacing={4}>
- 
-//       {post.map((post) => (
-//         <Grid key={post.id}>
-//           <CardActionArea component="a" href="#" sx={{maxWidth: "350px"}}>
-//             <Card sx={{ display: 'flex' }}>
-//               <CardContent sx={{ flex: 1 }}>
-//                 <Typography component="h2" variant="h5">
-//                   {post.name}
-//                 </Typography>
-//                 <Typography variant="subtitle1" color="text.secondary">
-//                   {post.date}
-//                 </Typography>
-//                 <Typography variant="subtitle1" paragraph>
-//                   {post.blurb}
-//                 </Typography>
-//                 <Typography variant="subtitle1" color="primary">
-//                   Continue reading...
-//                 </Typography>
-//               </CardContent>
-//               <CardMedia
-//                 component="img"
-//                 sx={{ width: 160, display: { xs: 'none', sm: 'block' } }}
-//                 src={`data:image/png;base64, ${post.image}`}
-//                 alt={`a screenshot of the webpage for project ${post.name}`}
-//               />
-//             </Card>
-//           </CardActionArea>
-//         </Grid>
-//       ))}
-//       </Grid>
-//     </Container>
-//   )
-// }
-
-
